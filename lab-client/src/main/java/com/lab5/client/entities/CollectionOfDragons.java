f39288d56d7f94cfb53b2607106a22108cf13bbe
Dmirtiy Busygin <Busygin.voll@mail.ru>
2022-02-22 02:22:08 +0300

    Create CollectionOfDragons.java

---
+++ lab-client/src/main/java/com/lab5/client/entities/CollectionOfDragons.java
@@ -0,0 +1,118 @@
+package com.lab5.client.entities;
+
+import java.io.File;
+import java.util.Date;
+import java.util.HashSet;
+
+/**
+ * Класс коллекции, содержащий текущую коллекцию <b>dragons</b>,
+ * отвечает за генерацию ID для новых элементов и за все действия,
+ * связанные с коллекцией.
+ */
+public class CollectionOfDragons {
+    /**
+     * Статичное поле, отвечающее за инкрементацию ID для исключения повторений
+     */
+    private static int idCounter = 1;
+    /**
+     * Дата создания коллекции
+     */
+    private final Date creationDate;
+    /**
+     * Файл, в который будет производиться запись коллекции при сохранении
+     */
+    private File outFile;
+    /**
+     * Сет объектов класса Dragon, текущее содержимое коллекции
+     */
+    private HashSet<Dragon> dragons;
+
+    public CollectionOfDragons() {
+        dragons = new HashSet<>();
+        creationDate = new Date();
+    }
+
+    public CollectionOfDragons(File outFile) {
+        this.outFile = outFile;
+        dragons = new HashSet<>();
+        creationDate = new Date();
+    }
+
+    /**
+     * Метод, возвращающий текущую коллекцию драконов в формате HashSet
+     *
+     * @return HashSet драконов, находящихся в коллекции
+     */
+    public HashSet<Dragon> getDragons() {
+        return dragons;
+    }
+
+    /**
+     * Метод, возвращающий дату создания коллекции
+     *
+     * @return дата создания коллекции
+     */
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    /**
+     * Метод, возвращающий текущий файл, в который будет производиться запись коллекции при сохранении
+     *
+     * @return файл, в который производится запись готовой коллекции
+     */
+    public File getOutFile() {
+        return outFile;
+    }
+
+    /**
+     * Метод, устанавливающий файл, в который будет производиться запись коллекции при сохранении
+     *
+     * @param outFile файл, в который будет производиться запись коллекции
+     */
+    public void setOutFile(File outFile) {
+        this.outFile = outFile;
+    }
+
+    /**
+     * Метод, добавляющий полученного дракона в коллекцию
+     *
+     * @param dragon дракон, которого нужно добавить в коллекцию
+     */
+    public void addDragon(Dragon dragon) {
+        dragon.setId();
+        idCounter++;
+        dragons.add(dragon);
+        System.out.println("Дракон успешно добавлен в коллекцию");
+    }
+
+    /**
+     * Метод, очищающий текущую коллекцию
+     */
+    public void clear() {
+        dragons.clear();
+    }
+
+    /**
+     * Метод, удаляющий дракона из коллекции по полученному ID, если таковой существует
+     *
+     * @param id id дракона, которого нужно удалить
+     */
+    public void removeById(long id) {
+        for (Dragon dragon : dragons) {
+            if (dragon.getId() == id) {
+                dragons.remove(dragon);
+                System.out.println("Dragon succesfully removed");
+            }
+        }
+    }
+
+    /**
+     * Метод, выводящий пользователю информацию о коллекции
+     */
+    public void showInfo() {
+        System.out.println("Collection type: " + dragons.getClass()
+                + " initialization date: " + creationDate
+                + " count of dragons: " + dragons.size());
+    }
+}
